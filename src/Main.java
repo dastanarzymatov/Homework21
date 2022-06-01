7a6ff645ac76f141015e3c5f136e3852ad56b487
dasta <arzymatodastan@gmail.com>
2022-06-01 11:58:27 +0600

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            FileWriter fileWriter = new FileWriter(" alfavit ");
+
+            for (char i = 'A'; i <= 'Z'; i++) {
+
+                for (char j = 'a'; j <= 'z'; j++) {
+                    if ((j - i) == 32) {
+                        fileWriter.append(i + "  " + j + "\n");
+                    }
+
+                }
+
+            }
+            for (char k = '0'; k <= '9'; k++) {
+                fileWriter.append(k + "\n");
+            }
+            fileWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+
+        }
+        try {
+            FileReader fileReader = new FileReader(" alfavit ");
+            Scanner scanner = new Scanner(fileReader);
+            int count=1;
+            while (scanner.hasNextLine()) {
+                System.out.println(count+") "+scanner.nextLine());
+                count++;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
     }
 }
